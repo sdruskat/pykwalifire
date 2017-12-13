5b2b016c2be6397c0fde8ff52f3a88863fb0b1d4
Stephan Druskat <mail@sdruskat.net>
2017-12-13 15:23:47 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,12 @@ except ImportError:
 
 with open('README.md') as f:
     readme = f.read()
-with open(os.path.join('docs', 'release-notes.rst')) as f:
-    history = f.read()
 
 setup(
     name="pykwalifire",
     version="1.7.0",
     description='Python lib/cli for JSON/YAML schema validation',
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author="Stephan Druskat",
     author_email="mail@sdruskat.net",
     maintainer='Stephan Druskat',
@@ -36,13 +34,15 @@ setup(
     ],
     classifiers=(
         # 'Development Status :: 1 - Planning',
-        'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
         # 'Development Status :: 4 - Beta',
-        # 'Development Status :: 5 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
         'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Environment :: Console',
@@ -51,5 +51,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     )
 )
