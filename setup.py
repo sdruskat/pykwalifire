ca6adb807835a9b0e6a249dbb54ae21f6bef42c5
Stephan Druskat <mail@sdruskat.net>
2017-12-18 11:43:46 +0100

    Set version 1.7.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name="pykwalifire",
-    version="1.7.0",
+    version="1.7.1",
     description='Python lib/cli for JSON/YAML schema validation, fork '
                 'of pykwalify with added features',
     long_description=readme,
